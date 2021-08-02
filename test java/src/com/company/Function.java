8fd5f9f2586fccb7405fc38defa7b2778a8698f7
Luigi Vismara <33238929+luigivis@users.noreply.github.com>
2021-08-01 20:45:49 -0500

    Functional
    Encrypt, descript complete

--- test java/src/com/company/Function.java
+++ test java/src/com/company/Function.java
@@ -2,47 +2,55 @@ package com.company;
 
 import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import java.util.Base64;
 
 public class Function {
 
-    public SecretKey generateKey(int n) throws NoSuchAlgorithmException, InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
-        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-        keyGenerator.init(n);
-        SecretKey originalKey = keyGenerator.generateKey();
-        System.out.println(originalKey);
-        IvParameterSpec ivParameterSpec = new IvParameterSpec("1234567891234567".getBytes(StandardCharsets.UTF_8));
-        encrypts("luigi", originalKey, ivParameterSpec);
-        return originalKey;
+    private SecretKeySpec crearClave(String clave) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        byte[] claveEncriptacion = clave.getBytes("UTF-8");
+
+        MessageDigest sha = MessageDigest.getInstance("SHA-1");
+
+        claveEncriptacion = sha.digest(claveEncriptacion);
+        claveEncriptacion = Arrays.copyOf(claveEncriptacion, 16);
+
+        SecretKeySpec secretKey = new SecretKeySpec(claveEncriptacion, "AES");
+
+        return secretKey;
     }
 
-    public static String encrypts(String input, SecretKey key,
-                                 IvParameterSpec iv) throws NoSuchPaddingException, NoSuchAlgorithmException,
-            InvalidAlgorithmParameterException, InvalidKeyException,
-            BadPaddingException, IllegalBlockSizeException {
+    public String encriptar(String datos, String claveSecreta) throws UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
+        SecretKeySpec secretKey = this.crearClave(claveSecreta);
 
-        Cipher cipher = Cipher.getInstance("AES");
-        cipher.init(Cipher.ENCRYPT_MODE, key, iv);
-        byte[] cipherText = cipher.doFinal(input.getBytes());
-        System.out.printf(Base64.getEncoder().encodeToString(cipherText));
-        return Base64.getEncoder().encodeToString(cipherText);
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
 
+        byte[] datosEncriptar = datos.getBytes("UTF-8");
+        byte[] bytesEncriptados = cipher.doFinal(datosEncriptar);
+        String encriptado = Base64.getEncoder().encodeToString(bytesEncriptados);
+
+        return encriptado;
     }
 
-    public static String decrypt(String cipherText, SecretKey key,
-                                 IvParameterSpec iv) throws NoSuchPaddingException, NoSuchAlgorithmException,
-            InvalidAlgorithmParameterException, InvalidKeyException,
-            BadPaddingException, IllegalBlockSizeException {
+    public String desencriptar(String datosEncriptados, String claveSecreta) throws UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
+        SecretKeySpec secretKey = this.crearClave(claveSecreta);
 
-        Cipher cipher = Cipher.getInstance("AES");
-        cipher.init(Cipher.DECRYPT_MODE, key, iv);
-        byte[] plainText = cipher.doFinal(Base64.getDecoder().decode(cipherText));
-        System.out.println(Base64.getDecoder().decode(cipherText));
-        return new String(plainText);
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+
+        byte[] bytesEncriptados = Base64.getDecoder().decode(datosEncriptados);
+        byte[] datosDesencriptados = cipher.doFinal(bytesEncriptados);
+        String datos = new String(datosDesencriptados);
+
+        return datos;
     }
 
 }
--- test java/src/com/company/Main.java
+++ test java/src/com/company/Main.java
@@ -1,24 +1,32 @@
 package com.company;
 
 import javax.crypto.*;
+import java.io.UnsupportedEncodingException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
 
 public class Main {
 
     public static void main(String[] args) throws NoSuchAlgorithmException, InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
-	// write your code here
-        double num1 = 5, num2 = 10;
-        double total = num1+num2;
-        for (int i = 1; i<= 15; i++) {
-            total = total * i;
-            System.out.println(total+" "+i);
-        }
+        try {
+            final String claveEncriptacion = "Hacker*_*";
+            String datosOriginales = "Mateo Aguilar";
 
-        Function function = new Function();
-        function.generateKey(128);
+            Function encriptador = new Function();
+
+            String encriptado = encriptador.encriptar(datosOriginales, claveEncriptacion);
+            String desencriptado = encriptador.desencriptar(encriptado, claveEncriptacion);
+
+            System.out.println("Cadena Original: " + datosOriginales);
+            System.out.println("Encriptado     : " + encriptado);
+            System.out.println("Desencriptado  : " + desencriptado);
+
+        } catch (NoSuchAlgorithmException | InvalidKeyException | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException | UnsupportedEncodingException ex) {
+
+        }
     }
 
 
